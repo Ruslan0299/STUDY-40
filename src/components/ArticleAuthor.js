98b17d373bbd593956446a1a67d3ad0df2a2bcd6
Gnatishin_Ruslan <gnatisin.ruslan1988@gmail.com>
2023-04-13 18:22:12 +0300

    jsx

---
+++ src/components/ArticleAuthor.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ArticleAuthor () {
+    return (
+        <div className="author">
+            <span>Author: Mike</span>
+            <span>Published: 06.12.2022</span>
+            <span>Theme: Video cards</span>
+        </div>
+    )
+}
+export default ArticleAuthor;
---
+++ src/components/ArticleBody.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ArticleBody() {
+  return (
+    <div className="article__body">
+      <h2>Article description:</h2>
+      <p>
+        NVIDIA on Azure is bringing AI, networking, and high-performance
+        computing to the enterprise.
+      </p>
+    </div>
+  );
+}
+export default ArticleBody;
